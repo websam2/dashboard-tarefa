cf21e73eab92f74a3038dd76607050862bbc199a
Samuel Z <websamrgt@gmail.com>
2024-09-26 16:18:20 -0300

    fix: correção nas importações do "ui" e acrescentado tratamento de erro no resultado do formulário.

--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -1,25 +1,43 @@
-"use client"
-import { useState } from "react";
-import { Label } from "@radix-ui/react-label";
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 import Image from "next/image";
 import teamChecklist from "../app/assets/team checklist.svg";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
+
+import { Label } from "@radix-ui/react-label";
+import { Button } from "@/components/ui/button";
 import {
 	Card,
 	CardContent,
 	CardDescription,
 	CardHeader,
 	CardTitle,
-} from "./ui/card";
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 export default function LoginPage() {
-	const [email, setEmail] = useState<string>("");
-	const [password, setPassword] = useState<string>("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const router = useRouter();
 
+	//acessando a página principal
+	useEffect(() => {
+		if (isLoggedIn) {
+			router.push("/tasklist");
+		}
+	}, [isLoggedIn, router]);
+
+	// acesso temporário, precisa alterar.
 	const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log({ email, password });
+		if (email === "exemplo@email.com" && password === "123") {
+			setIsLoggedIn(true);
+		} else {
+			setError("e-mail ou senha está inválido");
+		}
 	};
 
 	return (
@@ -35,7 +53,7 @@ export default function LoginPage() {
 							Entre com sua conta
 						</CardTitle>
 						<CardDescription>
-							Utilize seu e e-mail e senha para entrar na lista de tarefas.
+							Utilize seu e-mail e senha para entrar na lista de tarefas.
 						</CardDescription>
 					</CardHeader>
 					<CardContent>
@@ -58,10 +76,11 @@ export default function LoginPage() {
 									type="password"
 									value={password}
 									onChange={(e) => setPassword(e.target.value)}
-									placeholder="sua senha secreta"
+									placeholder="123"
 									required
 								/>
 							</div>
+							{error && <p className="text-red-500">{error}</p>}
 							<Button type="submit" className="mt-6 w-full">
 								Entrar
 							</Button>
